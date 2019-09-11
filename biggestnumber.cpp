80ec8e7dd343c0b2b098643b1795cc4e926dd587
lmmckinley1 <lmmckinley1@ecc-jaguar.csuchico.edu>
2019-09-11 16:42:12 -0700

    Find the largest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,22 +1,43 @@
-//Author:
+//Leah McKinley
 #include<iostream>
+
+using std::cout;
+using std::endl;
 
 int main()
 {
-  //declare variables here
+  int num1, num2;
 
-  std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
-
+  std::cout<<"Please enter a whole number:\n"; 
+  std::cin>>num1;
   std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+  std::cin>>num2;
+  if (num1>num2)
+  {
+          cout<<"First number "<<num1<<" is the largest ";
+
+  } 
+  else
+  { 
+          cout<<"Second number "<<num2<<" is the largest ";
+
+  } 
+
 
   //determine if first or second is bigger and store that in a third variable
 
 
   std::cout<<"Of those two numbers, the biggest is: ";
-  //print out the variable representing the biggest number
+ if (num1>num2) 
+ {
+        cout<<num1<<endl;
 
+ }  
+ else    
+ {  
+        cout<<num2<<endl;
+
+ }   
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
