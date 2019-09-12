20a9582231d9043bba136642f070f5c8d50bb565
lmmckinley1 <lmmckinley1@ecc-jaguar.csuchico.edu>
2019-09-11 17:00:47 -0700

    Biggest number pt. 2

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -13,20 +13,6 @@ int main()
   std::cout<<"Please enter another whole number:\n";
   std::cin>>num2;
   if (num1>num2)
-  {
-          cout<<"First number "<<num1<<" is the largest ";
-
-  } 
-  else
-  { 
-          cout<<"Second number "<<num2<<" is the largest ";
-
-  } 
-
-
-  //determine if first or second is bigger and store that in a third variable
-
-
   std::cout<<"Of those two numbers, the biggest is: ";
  if (num1>num2) 
  {
